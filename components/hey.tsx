8343a5804e1e562c49097b4ec17704e7da399693
Jonathan Le <jonathanle1111@gmail.com>
2023-03-29 02:26:55 -0500

    added name

---
+++ components/hey.tsx
@@ -0,0 +1,14 @@
+
+
+
+export default function Hey()
+{
+    return (      
+        <div className=" block text-center item-center mt-72">
+            <p className=" text-6xl text-blue-500 ">Hey I'm {'\n'}</p>
+            
+            <p className="font-bold font-sans text-7xl text-white">Jonathan Le</p>
+        
+        </div>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,26 @@
-import Counter from './Counter';
+
+import Head from 'next/head';
+import Hey from '@/components/hey';
+
 
 export default function Home() {
   return (
     <>
-      <Counter />
+      <Head>
+        <title>Jonathan Le Personal Webpage</title>
+        
+      </Head>
+   
+      <Hey/>
+      
     </>
   )
 }
+
+
+/*
+Made good progression today. Next goal: 
+- Add a nav bar 
+- Add an about me page 
+- Add personal project page 
+*/
