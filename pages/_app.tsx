3a4d842971125d230075f077e6b3566bcc02dde7
Jonathan Le <jonathanle1111@gmail.com>
2023-04-19 00:18:33 -0500

    remove necessary

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app';
-import SiteLayout from '@/components/common/SiteLayout';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
