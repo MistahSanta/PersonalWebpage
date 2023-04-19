444a8e8506033384a80b1161b4715fb723ecd8dc
Jonathan Le <jonathanle1111@gmail.com>
2023-04-19 00:37:36 -0500

    test

--- pages/aboutme.tsx
+++ pages/aboutme.tsx
@@ -14,7 +14,7 @@ export default function AboutMe() {
 
         <div className="flex-wrap-reverse sm:flex-nowrap flex flex-row h-full max-h-full mx-auto max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xl xl:max-w-3xl 2xl:max-w-4xl">
         <section className="flex items-center md:max-w-lg lg:max-w-xl">
-          <div className="text-white grid item-center sm:text-sm md:text-base xl:text-xl 2xl:text-2xl font-mono relative">
+          <div className="text-white grid item-center sm:text-sm md:text-base xl:text-xl 2xl:text-2xl font-mono relative text-center">
             <p>
               <br></br>
               Hello! Thanks for visiting my page! My name is Jonathan Le, and I
@@ -38,7 +38,7 @@ export default function AboutMe() {
         </section>
         {/*This is the picture of me */}
         {/* 1080 × 1616 */}
-          <section className="relative h-[50vh]  w-[40vw] sm:[50vw] my-auto mx-auto sm:max-h-max sm:ml-3 border-white border-4"> 
+          <section className="relative h-[50vh] min-w-[70vw] sm:min-w-[30vw] my-auto mx-auto sm:max-h-max sm:ml-6 border-white border-4"> 
             <Image
                 src={profilePic}
                 alt="Picture of me"
