2fa1d61cce1c48ba2b7b8a40249ab7a5cd558484
Jonathan Le <jonathanle1111@gmail.com>
2023-04-19 16:01:16 -0500

    fixed fonting

--- pages/hey.tsx
+++ pages/hey.tsx
@@ -8,7 +8,7 @@ export default function Hey() {
         className="flex flex-col radial-gradient h-screen w-full border-b-4 border-white"
       >
         <div className="flex flex-col h-full w-full justify-center items-center">
-          <p className=" italic text-7xl text-blue-500 z-10 Roboto font-semibold">
+          <p className=" italic text-4xl  sm:text-7xl text-blue-500 z-10 Roboto font-semibold">
             Hey! I&apos;m {"\n"}
           </p>
           <p className="z-10 font-bold text-7xl font-sans sm:text-9xl text-white text-center">
